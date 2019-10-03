87119e9358875973b2edb2ce76f86d4999b3474f
steviekong <sidharth.reji@gmail.com>
2019-10-03 12:59:05 -0400

    updated with client arguments

--- TSClient.py
+++ TSClient.py
@@ -1,34 +1,40 @@
 import socket
 import time
+import sys
 
-IP = 'localhost'
+IP = "0.0.0.0"
 PORT = 14666
 BUFFER_SIZE = 1024
 MESSAGE = None
 NUM_REQ = 5
 
+if len(sys.argv) is 2:
+	IP = sys.argv[1]
+
 def main():
 	STEP = 1
 	RTT = 0
-	REMOTE_TIME = 0
+	REMOTE_TIME = time.time()
+	offset = []
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((IP, PORT))
 	#print("Connected to server!")
 	while STEP <= NUM_REQ+1:
 		time.sleep(1)
 		#print(STEP)
-		(REMOTE_TIME, RTT )= sync(s, STEP, REMOTE_TIME, RTT)
+		(REMOTE_TIME, RTT)= sync(s, STEP, REMOTE_TIME, RTT)
+		#print(REMOTE_TIME)
 		STEP += 1
 	s.close()
 
 
 def sync(s, STEP, REMOTE_TIME, RTT):
 	if STEP > NUM_REQ:
-		print("REMOTE_TIME " + str(round(REMOTE_TIME)) +"\nLOCAL_TIME " + str(round(time.time()))+"\nRTT_ESTIMATE "+ str(round(RTT)))
+		print("REMOTE_TIME " + str(int(REMOTE_TIME)) +"\nLOCAL_TIME " + str(int(time.time()))+"\nRTT_ESTIMATE "+ str(int(RTT)))
 		return 1, 1
 	else:
 		MESSAGE = "STEP "+str(STEP) +"!"
-		t0 = time.time()
+		t0 = REMOTE_TIME
 		#print("sending data", MESSAGE, "At time:", t0)
 		s.send(MESSAGE.encode())
 		data = None
@@ -38,7 +44,7 @@ def sync(s, STEP, REMOTE_TIME, RTT):
 				break 
 		#print("recieved data from server:", data)
 		if data is not None:
-			t3 = time.time()
+			t3 = REMOTE_TIME + (time.time() - t0)
 			#print(data, t3)
 			split = data.decode().split(' ')
 			t1 = float(split[3])
@@ -46,8 +52,13 @@ def sync(s, STEP, REMOTE_TIME, RTT):
 			RTT = (t3 - t0) - (t2 - t1)
 			OFFSET = ((t1 - t0) + (t2 - t3))/2
 			#print("RTT is :" + str(RTT) + " and OFFSET is " + str(OFFSET))
-			return (time.time() + OFFSET, RTT)
-		return 1
+			if REMOTE_TIME > t2 + OFFSET:
+				return (t3 + OFFSET, RTT)
+			else:
+				return (t3 - OFFSET, RTT)
+		
+	
+		return 1, 1, 1	
 
 if __name__ == "__main__":
 	main()
